ebbd31d6223af864aec16aa629cf9af5c68d779a
Xasper2b2t <iiodklyou.were@gmail.com>
2026-03-14 18:45:34 +0300

    homework

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 import telebot # библиотека telebot
 from config import token # импорт токена
-
+import random
 bot = telebot.TeleBot(token) 
 
 @bot.message_handler(commands=['start'])
@@ -31,4 +31,39 @@ def check_link_and_ban_user(message):
             bot.send_message(chat_id=message.chat.id,text=f"Пользователь {message.from_user.first_name} ({user_id}) забанен за публикацию ссылки.")
         except Exception as e:
             print(f"Ошибка при попытке забанить пользователя:  {e}")
+@bot.message_handler(content_types=['new_chat_members'])
+def handle_new_chat_member(message):
+    for member in message.new_chat_members:
+        bot.send_message(message.chat.id, f'Приветствуем нового участника: {member.first_name}')
+        bot.approve_chat_join_request(message.chat.id, member.id)
+
+@bot.message_handler(commands=['quote'])
+def quote_handler(message):
+    quotes = random.choice(['«Обстоятельства часто можно изменить, изменив своё отношение к ним» — Элеонора Рузвельт. ',
+
+    '«Мы не можем решить проблемы тем же мышлением, которое использовали, когда их создавали» — Альберт Эйнштейн.',
+    '«Учись так, будто будешь жить вечно, живи так, будто умрёшь завтра» — Махатма Ганди. ',
+    '«Когда ты даришь радость другим людям, ты получаешь больше радости взамен. Ты должен хорошо подумать о счастье, которое можешь дарить» — Элеонора Рузвельт.', 
+    '«Когда меняешь свои мысли, не забудь также изменить свой мир» — Норман Винсент Пил. '
+    '«Более полная жизнь не у того, кто прожил дольше, а у того, кто больше узнал» — Жан-Жак Руссо',
+    '«Счастье не в том, чтобы делать всегда, что хочешь, а в том, чтобы всегда хотеть того, что делаешь» — Лев Толстой. ',
+    '«Выживает не самый сильный из видов и не самый умный, а тот, кто лучше других реагирует на изменения» — Леон Меггинсон. '])
+    bot.reply_to(message, quotes)
+
+
+@bot.message_handler(commands=['coin'])
+def coin_handler(message):
+    coin = random.choice(["ОРЕЛ", "РЕШКА"])
+    bot.reply_to(message, coin)
+
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['info'])
+def send_info(message):
+    bot.reply_to(message,'Здравтвуте это телеграм бот сделанный на курсе Kodland PythonLVL3')
+
+# Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
 bot.infinity_polling(none_stop=True)
